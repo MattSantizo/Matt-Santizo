bd5cd211376217125ccc667c5802d06e51d37d0c
MattSantizo <lilkidstatus@gmail.com>
2017-01-26 13:56:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-$( ".cross" ).hide();
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".cross" ).show();
-});
+$(".left-side").on("click", function(){
+    $(".sidebar").addClass("move-right");
 });
 
-$( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".cross" ).hide();
-$( ".hamburger" ).show();
-});
+$(".close").on("click", function(){
+  $(".sidebar").removeClass("move-right");
 });
 
-var navBarPos = $(".navbar").offset().top;
-  
-  $(window).scroll(function(){
+$("#contact").on("click", function(){
+  $(".contact-me").addClass("move-down");
+})
 
-    var scrollP = $(this).scrollTop();
-    if (scrollP >= navBarPos) {
-      $(".navbar").addClass("sticky");
-      $(".navbar").css('opacity', '0.95');
-
-    } else {
-      $(".navbar").removeClass("sticky");
-      $(".navbar").css('opacity', '1');
-
-    }
-  });
-
-// Hamburger Menu from http://codepen.io/g13nn/pen/eHGEF
+$(".contact-close").on("click", function(){
+  $(".contact-me").removeClass("move-down");
+});
